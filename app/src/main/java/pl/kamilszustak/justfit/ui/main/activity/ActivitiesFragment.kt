88d658c5fb0c58907cd9805cfd14dbc90cdce0c9
Kamil Szustak <kamil.szustak98@gmail.com>
2020-06-03 20:50:54 +0200

    Add activities list item layout; #11

--- app/src/main/java/pl/kamilszustak/justfit/ui/main/activity/ActivitiesFragment.kt
+++ app/src/main/java/pl/kamilszustak/justfit/ui/main/activity/ActivitiesFragment.kt
@@ -1,6 +1,26 @@
 package pl.kamilszustak.justfit.ui.main.activity
 
+import android.app.Activity
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
+import com.mikepenz.fastadapter.adapters.ModelAdapter
+import pl.kamilszustak.justfit.databinding.FragmentActivitiesBinding
+import pl.kamilszustak.justfit.domain.item.EmployeeItem
+import pl.kamilszustak.justfit.domain.model.employee.Employee
 import pl.kamilszustak.justfit.ui.base.BaseFragment
+import javax.inject.Inject
 
 class ActivitiesFragment : BaseFragment() {
+    @Inject
+    protected lateinit var viewModelFactory: ViewModelProvider.AndroidViewModelFactory
+    private val viewModel: ActivitiesViewModel by viewModels {
+        viewModelFactory
+    }
+
+    private lateinit var binding: FragmentActivitiesBinding
+    private val modelAdapter: ModelAdapter<Activity, ActivityItem> by lazy {
+        ModelAdapter<Activity, ActivityItem> { activity ->
+            ActivityItem(activity)
+        }
+    }
 }
--- app/src/main/java/pl/kamilszustak/justfit/ui/main/activity/ActivitiesViewModel.kt
+++ app/src/main/java/pl/kamilszustak/justfit/ui/main/activity/ActivitiesViewModel.kt
@@ -1,10 +1,22 @@
 package pl.kamilszustak.justfit.ui.main.activity
 
 import android.app.Application
+import pl.kamilszustak.justfit.common.livedata.ResourceDataSource
+import pl.kamilszustak.justfit.domain.model.activity.Activity
+import pl.kamilszustak.justfit.domain.usecase.activity.GetAllActivitiesUseCase
 import pl.kamilszustak.justfit.ui.base.BaseViewModel
 import javax.inject.Inject
 
 class ActivitiesViewModel @Inject constructor(
-    application: Application
+    application: Application,
+    private val getAllActivities: GetAllActivitiesUseCase
 ) : BaseViewModel(application) {
+
+    val activitiesResource: ResourceDataSource<List<Activity>> = ResourceDataSource()
+
+    init {
+        activitiesResource.setFlowSource {
+            getAllActivities()
+        }
+    }
 }
